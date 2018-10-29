e6e4c63c472c63ef0220cf91b815e2045d48f152
TaylorCarrick <tlc6w@mtmail.mtsu.edu>
2018-10-28 19:36:25 -0500

    added javascript to set the date for updates

---
+++ myScript.js
@@ -0,0 +1,4 @@
+
+var d = new Date();
+d.setFullYear(2018, 10, 28);
+document.getElementsByTagName("footer")[0].getElementsByTagName("small")[0].innerHTML= "Taylor Carrick Last Updated: " + d + " Currently Under Construction";
