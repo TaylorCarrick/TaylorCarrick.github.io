ac7202b633485391c753a45b2522ca5c8616449c
TaylorCarrick <tlc6w@mtmail.mtsu.edu>
2018-10-28 19:40:54 -0500

    moved slides script to external javascript file

--- myScript.js
+++ myScript.js
@@ -2,3 +2,30 @@
 var d = new Date();
 d.setFullYear(2018, 10, 28);
 document.getElementsByTagName("footer")[0].getElementsByTagName("small")[0].innerHTML= "Taylor Carrick Last Updated: " + d + " Currently Under Construction";
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
